53a69c6c9103292ef4db6f08e7ffc9143b7a7ba3
严加薪 <yanjiaxin.0713@bytedance.com>
2024-11-04 16:21:56 +0800

    feat:add textfield

--- UITest/UITest/Friends/FriendsVC.m
+++ UITest/UITest/Friends/FriendsVC.m
@@ -8,6 +8,13 @@
 #import "FriendsVC.h"
 #import "TestTouchView.h"
 
+@interface FriendsVC () <UITextFieldDelegate, UIGestureRecognizerDelegate>
+
+@property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) UITapGestureRecognizer *tabGestureRecognizer;
+
+@end
+
 @implementation FriendsVC
 
 - (void)viewDidLoad
@@ -19,6 +26,8 @@
     TestTouchView *viewB = [[TestTouchView alloc] initWithFrame:CGRectMake(50, 50, 300, 200) name:@"viewB"];
     viewB.backgroundColor = [UIColor blueColor];
     [viewA addSubview:viewB];
+    
+    [viewA addSubview:self.textField];
     
     TestTouchView *viewC = [[TestTouchView alloc] initWithFrame:CGRectMake(50, 400, 300, 300) name:@"viewC"];
     viewC.backgroundColor = [UIColor grayColor];
@@ -40,6 +49,58 @@
 //    viewG.backgroundColor = [UIColor greenColor];
 //    [viewB addSubview:viewG];
 
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self.textField becomeFirstResponder];
+}
+
+- (UITextField *)textField
+{
+    if (!_textField){
+        _textField = [[UITextField alloc] initWithFrame:CGRectMake(50, 300, 300, 80)];
+        _textField.backgroundColor = [UIColor whiteColor];
+        _textField.delegate = self;
+        
+        self.tabGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard:)];
+        self.tabGestureRecognizer.delegate = self;
+        [self.view addGestureRecognizer:self.tabGestureRecognizer];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(keyboardWillShow:)
+                                                     name:UIKeyboardWillShowNotification
+                                                   object:nil];
+        
+    }
+    return _textField;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField 
+{
+
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification 
+{
+    NSDictionary *userInfo = [notification userInfo];
+    CGRect keyboardFrame = [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    NSTimeInterval animationDuration = [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    NSLog(@"keyboardWillShow notification : %@",userInfo);
+}
+
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+#pragma mark - tabGestureRecognizer
+- (void)hideKeyboard:(UITapGestureRecognizer *)gesture
+{
+    [self.view endEditing:YES];
 }
 
 @end
